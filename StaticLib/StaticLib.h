35b8bb957159282aa375ce3f30f9c07efbba5a27
ASSASSINYM <darlingiloveu@sina.com>
2021-01-22 17:05:49 +0800

    Initial Commit

---
+++ StaticLib/StaticLib.h
@@ -0,0 +1,12 @@
+//
+//  StaticLib.h
+//  StaticLib
+//
+//  Created by ASSASSIN on 2021/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLib : NSObject
+
+@end
---
+++ StaticLib/StaticLib.m
@@ -0,0 +1,12 @@
+//
+//  StaticLib.m
+//  StaticLib
+//
+//  Created by ASSASSIN on 2021/1/22.
+//
+
+#import "StaticLib.h"
+
+@implementation StaticLib
+
+@end
