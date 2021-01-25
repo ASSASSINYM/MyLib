eb840798b248fb884716a25cdfbebdb80313513b
ASSASSINYM <darlingiloveu@sina.com>
2021-01-25 10:05:26 +0800

    静态库打包 注意事项

---
+++ StaticLib/StaticLibTool.h
@@ -0,0 +1,18 @@
+//
+//  StaticLibTool.h
+//  StaticLib
+//
+//  Created by ASSASSIN on 2021/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StaticLibTool : NSObject
+
++(void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StaticLib/StaticLibTool.m
@@ -0,0 +1,16 @@
+//
+//  StaticLibTool.m
+//  StaticLib
+//
+//  Created by ASSASSIN on 2021/1/25.
+//
+
+#import "StaticLibTool.h"
+
+@implementation StaticLibTool
+
++(void)test {
+    NSLog(@"this is static lib test obj");
+}
+
+@end
